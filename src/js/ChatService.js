843a6a11b6e5b460d07fb894fe55fb26accc85dc
MaximGromak <maximgromak@gmail.com>
2022-01-28 20:03:41 +1000

    add: sending chat msgs

--- src/js/ChatService.js
+++ src/js/ChatService.js
@@ -3,14 +3,60 @@ export default class ChatService {
     constructor(user) {
         this.user = user;
         this.addUserToPanel();
+        this.setNewMessageSendFunction();
     }
 
 
-    setNewMessageSendFunction(){
-        const inputFieldElem = document.getElementsByClassName('chat-window-type-here')[0];
+    setNewMessageSendFunction() {
+        const textArea = document.getElementsByClassName('chat-window-type-here')[0];
+        textArea.addEventListener('keypress', (event) => {
+            if (event.key === 'Enter') {
+                console.log('enter pressed! ' + textArea.value);
+                this.showMsgInChat();
+            }
+        })
     }
 
-    async addUserToPanel(){
+    async showMsgInChat() {
+        const chatElem = document.getElementsByClassName('chat-window-log')[0];
+        const newMsg = await this.buildNewMessage();
+        console.log(newMsg)
+
+        chatElem.children[0].before(newMsg);
+
+        const textArea = document.getElementsByClassName('chat-window-type-here')[0];
+        textArea.value = "";
+    }
+
+    async buildNewMessage() {
+        const textArea = document.getElementsByClassName('chat-window-type-here')[0];
+
+        const chatMessageElem = document.createElement('div');
+        chatMessageElem.classList.add('chat-message');
+        chatMessageElem.classList.add('owner');
+
+        const messageInfoElem = document.createElement('div');
+        messageInfoElem.classList.add('chat-message-info');
+        const fromElem = document.createElement('div');
+        fromElem.classList.add('message-from');
+        fromElem.textContent = this.user.getNickname();
+        messageInfoElem.appendChild(fromElem);
+        const msgDateElem = document.createElement('div');
+        msgDateElem.classList.add('message-date');
+        messageInfoElem.appendChild(msgDateElem);
+
+        chatMessageElem.appendChild(messageInfoElem);
+
+        const msgContentElem = document.createElement('div');
+        msgContentElem.classList.add('message-content');
+        msgContentElem.textContent = textArea.value;
+
+        chatMessageElem.appendChild(msgContentElem);
+
+        return chatMessageElem;
+    }
+
+    async addUserToPanel() {
         const htmlUser = await this.buildNewUser(this.user);
         document.getElementsByClassName('contacts')[0].children[0].after(htmlUser);
     }
